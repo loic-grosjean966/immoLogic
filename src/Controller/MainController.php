332a48aed8fd617970c993740c334292b9108a48
Stevie <steven.guidat.sg711@gmail.com>
2020-05-28 12:31:47 +0200

    redirection terminé

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/accueil", name="accueil")
      */
     public function index()
     {
