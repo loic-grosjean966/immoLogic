ee692478d79b84c2472728b0fbd2c73467169ef9
Stevie <steven.guidat.sg711@gmail.com>
2020-05-28 11:59:49 +0200

    Redirection page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,4 +16,40 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+    /**
+     * @Route("/Vente-Maison", name="houseSell")
+     */
+    public function houseSell()
+    {
+        return $this->render('house/houseSell.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/Location-Maison", name="houseRent")
+     */
+    public function houseRent()
+    {
+        return $this->render('house/houseRent.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/Location-Appartement", name="appartmentRent")
+     */
+    public function appartementRent()
+    {
+        return $this->render('appartment/appartmentRent.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/Vente-Appartement", name="appartmentSell")
+     */
+    public function appartmentSell()
+    {
+        return $this->render('appartment/appartmentSell.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
 }
